6070a1669608945cf7f7aad200d724f3e8238e42
April Rieger <april.rieger@yahoo.com>
2020-05-04 08:35:33 -0700

    update

--- testing-with-jest.test.js
+++ testing-with-jest.test.js
@@ -8,7 +8,7 @@
 
 // --------------------1a) Create a test for a function called addThemUp that takes two numbers as arguments and returns the sum.
 
-describe a function that takes an argument that adds two numbers
+// describe a function that takes an argument that adds two numbers
 describe("addThemUp", () => {
   //test that the outcome is as expected
   test('adds 2+2 to equal 4', () => {
@@ -25,8 +25,11 @@ console.log(2, 2)
 
 // --------------------2a) Create a test for a function called triangleArea that takes the base and the height of a triangle and returns the area. The area of a triange is the base times the height divided by two.
 
+// describe a function that takes the base and the height of a triangle and returns the area
 describe ("triangleArea", () => {
+  //test that the outcome is as expected
   test(`Takes the base and height of a triangle and outputs the area of the triangle`, () => {
+    //expect the outcome
     expect(triangleArea(10,5)).toEqual(25)
   })
 })
